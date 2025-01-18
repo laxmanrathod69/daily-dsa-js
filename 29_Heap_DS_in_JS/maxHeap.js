a041b35bfd894f66c57aa46699b2657c8af3c021
Laxman Rathod <laxmanrathod.dev@gmail.com>
2025-01-19 00:09:27 +0530

    max heap javascript

---
+++ 29_Heap_DS_in_JS/maxHeap.js
@@ -0,0 +1,83 @@
+class MinHeap {
+  constructor() {
+    this._heap = [];
+  }
+  getParentIndex(index) {
+    return Math.floor((index - 1) / 2);
+  }
+  getLeftChildIndex(index) {
+    return 2 * index + 1;
+  }
+  getRightChildIndex(index) {
+    return 2 * index + 2;
+  }
+  swap(index1, index2) {
+    [this._heap[index1], this._heap[index2]] = [
+      this._heap[index2],
+      this._heap[index1],
+    ];
+  }
+  peek() {
+    return this._heap[0];
+  }
+  size() {
+    return this._heap.length;
+  }
+  // Inserting a value
+  insert(value) {
+    this._heap.push(value);
+    this.bubbleUp();
+  }
+
+  bubbleUp() {
+    let index = this._heap.length - 1;
+    while (
+      index > 0 &&
+      this._heap[index] > this._heap[this.getParentIndex(index)]
+    ) {
+      this.swap(index, this.getParentIndex(index));
+      index = this.getParentIndex(index);
+    }
+  }
+
+  // Removing the Root Element
+  remove() {
+    if (this.size() === 0) return null;
+    if (this.size() === 1) return this._heap.pop();
+
+    const root = this._heap[0];
+    this._heap[0] = this._heap.pop();
+    this.bubbleDown();
+    return root;
+  }
+
+  bubbleDown() {
+    let index = 0;
+    while (this.getLeftChildIndex(index) < this.size()) {
+      let largerChildIndex = this.getLeftChildIndex(index);
+      if (
+        this.getRightChildIndex(index) < this.size() &&
+        this._heap[this.getRightChildIndex(index)] >
+          this._heap[largerChildIndex]
+      ) {
+        largerChildIndex = this.getRightChildIndex(index);
+      }
+
+      if (this._heap[index] > this._heap[largerChildIndex]) break;
+
+      this.swap(index, largerChildIndex);
+      index = largerChildIndex;
+    }
+  }
+}
+
+const minHeap = new MinHeap();
+
+minHeap.insert(10);
+minHeap.insert(5);
+minHeap.insert(20);
+minHeap.insert(3);
+
+console.log(minHeap.peek()); // Output: 20
+console.log(minHeap.remove()); // Output: 20
+console.log(minHeap.peek()); // Output: 10
