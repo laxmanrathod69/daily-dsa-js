ca7762a93b5d20d947e50d6869f3f6c451a8b1ea
Laxman Rathod <laxmanrathod.dev@gmail.com>
2025-03-18 23:53:43 +0530

    Implement Quick Sort

---
+++ 45_Quick_Sort/tempCodeRunnerFile.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} arr
+ * @param {number} low
+ * @param {number} high
+ */
+
+class Solution {
+  partition(arr, low, high) {
+    const pivot = arr[low];
+    let i = low;
+    let j = high;
+
+    while (i < j) {
+      while (arr[i] <= pivot && i < high) i++;
+      while (arr[j] > pivot && j > low) j++;
+
+      if (i < j) [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+
+    [pivot, arr[j]] = [arr[j], pivot];
+    return j;
+  }
+
+  quickSort(arr, low, high) {
+    if (low < high) {
+      const pivot = this.partition(arr, low, high);
+      this.quickSort(arr, low, pivot - 1);
+      this.quickSort(arr, pivot + 1, high);
+    }
+  }
+}
+
+const qs = new Solution();
+const arr = [4, 1, 3, 9, 7];
+qs.quickSort(arr, 0, arr.length - 1);
+console.log(qs); // Output: [1, 3, 4, 7, 9]
